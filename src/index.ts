e91f2d0b88c547bc5ca7f9181a59921ff9632f86
ArkinSolomon <arkin@arkinsolomon.net>
2023-06-10 22:36:27 -0500

    Got basic job service (needs testing)

--- src/index.ts
+++ src/index.ts
@@ -13,19 +13,172 @@
  * either express or implied limitations under the License.
  */
 
+/**
+ * The different type of jobs.
+ * 
+ * @name JobType
+ * @enum {string}
+ */
+enum JobType {
+  Packaging = 'packaging',
+  Resource = 'resource'
+}
+
+/**
+ * Data sent by the worker about it's job.
+ * 
+ * @typedef {Object} JobData
+ * @property {JobType} jobType The type of the job.
+ * @property {PackagingInfo|ResourceInfo} info The information about the job.
+ */
+type JobData = {
+  jobType: JobType;
+  info: PackagingInfo | ResourceInfo;
+}
+
+/**
+ * Information about a packaging job.
+ * 
+ * @typedef {Object} PackagingInfo
+ * @property {string} packageId The id of the package being processed.
+ * @property {string} version The version of the package being processed.
+ */
+export type PackagingInfo = {
+  packageId: string;
+  version: string;
+}
+
+/**
+ * Information about a resource job.
+ * 
+ * @typedef {Object} ResourceInfo
+ * @property {string} resourceId The id of the resource being processed.
+ */
+export type ResourceInfo = {
+  resourceId: string;
+};
+
+import dotenv from 'dotenv';
+dotenv.config();
+
 import http from 'http';
 import Express from 'express';
 import socketio from 'socket.io';
+import logger from './logger.js';
+import hasha from 'hasha';
+import * as jobDatabase from './jobDatabase.js';
+
+logger.info('X-Pkg jobs service starting');
 
 const app = Express();
 const server = http.createServer(app);
 const io = new socketio.Server(server);
 
 io.on('connection', client => {
-  console.log('New websocket connection');
+  const clientLogger = logger.child({ ip: client.conn.remoteAddress });
+  clientLogger.info('New connection');
+
+  let authorized = false;
+  let jobsDone = false;
+
+  let jobData: JobData;
+
+  // Dual password authorization for extra security
+  client.emit('handshake', process.env.SERVER_TRUST_KEY);
+
+  client.on('handshake', key => {
+    if (!key || typeof key !== 'string') {
+      client.disconnect();
+      clientLogger.info('No password provided or invalid type');
+      return;
+    }
+
+    const hashed = hasha(key, { algorithm: 'sha256' });
+    if (key === hashed) {
+      authorized = true;
+      logger.emit('Client authorized');
+      client.emit('authorized');
+    }
+  });
+
+  client.on('job_data', async (data: JobData) => {
+    if (!authorized) {
+      client.disconnect();
+      clientLogger.info('Client attempted to send job data when incomplete');
+      return;
+    }
+
+    jobData = data;
+    clientLogger.setBindings(data);
+    clientLogger.info('Client data recieved');
+
+    switch (data.jobType) {
+    case JobType.Packaging: {
+      const jobInfo = data.info as PackagingInfo;
+      await jobDatabase.addPackagingJob(jobInfo.packageId, jobInfo.version);
+      break;
+    }
+    // case JobType.Resource:
+    //   const jobInfo = data.info as ResourceInfo;
+    // break;
+    default:
+      clientLogger.warn('Invalid job type: ' + data.jobType);
+      client.disconnect();
+      return;
+    }
+
+    clientLogger.info('Job type registered on database');
+  });
+
+  client.on('done', async () => {
+    jobsDone = true;
+    clientLogger.info('Job completed');
+
+    switch (jobData.jobType) {
+    case JobType.Packaging: {
+      const jobInfo = jobData.info as PackagingInfo;
+      await jobDatabase.completePackagingJob(jobInfo.packageId, jobInfo.version);
+      break;
+    }
+    // case JobType.Resource:
+    //   const jobInfo = data.info as ResourceInfo;
+    // break;
+    default:
+      clientLogger.error('Invalid job type (while completing)');
+      client.disconnect();
+      return;
+    }
+  });
+
+  client.on('disconnect', async () => {
+    if (!authorized || jobsDone)
+      return;
+
+    clientLogger.info('Unexpected disconnect, attempting to set job as failure');
+    let didFail;
+    switch (jobData.jobType) {
+    case JobType.Packaging: {
+      const jobInfo = jobData.info as PackagingInfo;
+      didFail = await jobDatabase.failPackagingJob(jobInfo.packageId, jobInfo.version);
+      break;
+    }
+    // case JobType.Resource:
+    //   const jobInfo = data.info as ResourceInfo;
+    // break;
+    default:
+      clientLogger.error('Invalid job type (while failing)');
+      client.disconnect();
+      return;
+    }
+
+    if (didFail)
+      clientLogger.info('Failed job');
+    else
+      clientLogger.info('Job already had different status, was not failed');
+  });
 });
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5027;
 server.listen(port, () => {
   console.log(`Server is up on port ${port}!`);
 });
---
+++ src/jobDatabase.ts
@@ -0,0 +1,109 @@
+/*
+ * Copyright (c) 2023. Arkin Solomon.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied limitations under the License.
+ */
+
+import mysql from 'mysql2';
+import { PackagingInfo } from './index.js';
+
+const pool = mysql.createPool({
+  connectionLimit: 15,
+  host: '127.0.0.1',
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: 'xpkg_packages',
+  multipleStatements: false
+});
+
+/**
+ * Execute a query string.
+ * 
+ * @param {string} queryString The query string to execute.
+ * @returns {Promise<unknown[]>} A promise which resolves to the result of the query data, or rejects if the operation fails.
+ */
+function query(queryString: string): Promise<unknown[]> {
+  return new Promise((resolve, reject) => {
+    pool.getConnection((err, connection) => {
+      if (err)
+        return reject(err);
+
+      connection.query(queryString, (err, data) => {
+        connection.release();
+        if (err)
+          return reject(err);
+
+        resolve(data as unknown[]);
+      });
+      connection.on('error', reject);
+    });
+  });
+}
+
+/**
+ * Register a job that packages a specific package at a version.
+ * 
+ * @async
+ * @param {string} packageId The id of the package being packaged.
+ * @param {string} version The version being packaged.
+ * @returns {Promise<void>} A promise which resolves if the operation completes successfully.
+ */
+export async function addPackagingJob(packageId: string, version: string): Promise<void> {
+  await query(mysql.format('INSERT INTO package_processing_jobs (packageId, version) VALUES (?, ?);', [packageId, version]));
+}
+
+/**
+ * Complete a packaging job.
+ * 
+ * @async
+ * @param {string} packageId The id of the package who's job is complete.
+ * @param {string} version The version of the package who's job is complete.
+ * @returns {Promise<void>} A promise which resolves if the operation completes successfully.
+ */
+export async function completePackagingJob(packageId: string, version: string) {
+  return query(mysql.format('DELETE FROM package_processing_jobs WHERE packageId=? AND version=?;', [packageId, version]));
+}
+
+/**
+ * Fail a packaging job.
+ * 
+ * @async
+ * @param {string} packageId The id of the package who's job is being failed.
+ * @param {string} version The version of the package who's job is being failed.
+ * @returns {Promise<boolean>} A promise which resolves to true if the job was set as failed, or false if a different status was already inserted into the database.
+ */
+export async function failPackagingJob(packageId: string, version: string): Promise<boolean> {
+  const [, status] = await Promise.all([
+    completePackagingJob(packageId, version),
+    query(mysql.format('SELECT status FROM versions WHERE packageId=? AND version=? LIMIT 1;'))
+  ]);
+
+  // We always expect the job to be there, so this shouldn't fail
+  const jobStatus = (status as { status: string; }[])[0].status;
+
+  // Only update the job if it's processing, otherwise assume it's finished
+  if (jobStatus === 'processing') {
+    await query(mysql.format('UPDATE versions SET status=? WHERE packageId=? AND version=?;', ['failed_server', packageId, version]));
+    return true;
+  }
+  return false;
+}
+
+/**
+ * Get all packaging jobs.
+ * 
+ * @async
+ * @returns {Promise<PackagingInfo[]>} A promise which resolves to all of hte information of all packaging jobs.
+ */
+export async function getAllPackagingJobs(): Promise<PackagingInfo[]> {
+  return (await query('SELECT packageId, version FROM package_processing_jobs;')) as PackagingInfo[];
+}
